b8099a0bbc1e1185a4e1bd82d7f5e9263b981694
Dheeraj Edcast <dheeraj@edcast.com>
2026-01-08 17:45:26 -0800

    fix release issues while building release

--- scripts/package.js
+++ scripts/package.js
@@ -1,6 +1,10 @@
-const fs = require('fs');
-const path = require('path');
-const { execSync } = require('child_process');
+import fs from 'fs';
+import path from 'path';
+import { execSync } from 'child_process';
+import { createRequire } from 'module';
+import { fileURLToPath } from 'url';
+
+const require = createRequire(import.meta.url);
 const packageJson = require('../package.json');
 
 // Parse arguments
